f8a218a13221547fdfee005458446cfda626b251
97th Floor <97thfloor@97ths-MacBook-Pro-6.local>
2024-10-19 21:05:33 -0600

    class information page

--- docs/src/ClassInfoDisplay.jsx
+++ docs/src/ClassInfoDisplay.jsx
@@ -15,16 +15,36 @@ function displayClass() {
 
 function showClassSummary() {
     return (
-        <div>
-            <h1>Class Info</h1>
+        <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
+            <h1>CS 111: Intro to Computer Science</h1>
+            <p>Teaches how to design, develop, reason about, and test programs. 
+                Topics include higher-order functions, object-oriented programming, 
+                recursion, algorithms, data structures, decomposition, interpreters, 
+                and regular expressions.</p>
+
+            <div className="flex-container">
+                <h2 style={{ flex: '1' }}>Pre-Requisite Courses:</h2>
+                <p style={{ flex: '3', margin: 0 }}>None</p>
+            </div>
+
+            <div className="flex-container">
+                <h2 style={{ flex: '1' }}>When Taught:</h2>
+                <p style={{ flex: '3', margin: 0 }}>Fall, Winter, Spring, Summer</p>
+            </div>
         </div>
     );
 }
 
+
 function showPotentialTracks() {
     return (
-        <div>
+        <div style={{ padding: '20px'}}>
             <h1>Potential Tracks</h1>
+            <div class="link-container">
+                <div class="link"><a href="link1.html">Human Computer Interaction</a></div>
+                <div class="link"><a href="link2.html">Software Engineering</a></div>
+                <div class="link"><a href="link3.html">Machine Learning and Artificial Intelligence</a></div>
+            </div>
         </div>
     );
 }

7ecbba00fae4c3ff325cbe8b15aecdc524b90d17
kamea99 <kameanw99@gmail.com>
2024-11-09 15:00:56 -0700

    altered the sidebar to be individual buttons

--- docs/src/Sidebar.js
+++ docs/src/Sidebar.js
@@ -1,11 +1,11 @@
+import './Sidebar.css';
 import React, { useState } from 'react';
 
-const Sidebar = ({ onOptionSelect, onValueSelect }) => {
+/*const Sidebar = ({ onOptionSelect, onValueSelect }) => {
     const [selectedOption, setSelectedOption] = useState(null);
     const [selectedValue, setSelectedValue] = useState('');
   
     const options1 = initialize_required_classes()
-
     const options2 = initialize_elective_classes()
 
     const handleOptionSelect = (option) => {
@@ -50,47 +50,6 @@ const Sidebar = ({ onOptionSelect, onValueSelect }) => {
     );
 
 };
-    
-
-// function SidebarDrop() {
-//     const [selectedValue1, setSelectedValue1] = useState('');
-//     const [selectedValue2, setSelectedValue2] = useState('');
-
-//     const options1 = initialize_required_classes()
-
-//     const options2 = initialize_elective_classes()
-
-//     return (
-//         <><div className="sidebar">
-//             <div className="dropdown">
-//                 <h3>Required Classes:</h3>
-//                 <select
-//                     value={selectedValue1}
-//                     onChange={(e) => setSelectedValue1(e.target.value)}>
-
-//                     {options1.map((option) => (
-//                         <option key={option.value} value={option.value}>
-//                             {option.label}
-//                         </option>
-//                     ))}
-//                 </select>
-//             </div>
-//             <div className="dropdown">
-//                 <h3>Elective Classes:</h3>
-//                 <select
-//                     value={selectedValue2}
-//                     onChange={(e) => setSelectedValue2(e.target.value)}>
-
-//                     {options2.map((option) => (
-//                         <option key={option.value} value={option.value}>
-//                             {option.label}
-//                         </option>
-//                     ))}
-//                 </select>
-//             </div>
-//         </div></>
-//     );
-// }
 
 function initialize_required_classes() {
     var classes = [];
@@ -98,7 +57,7 @@ function initialize_required_classes() {
 
         for(let i = 0; i < info.length; i++) {
             classes.push(
-                { value: info[i]['class_code'], label: info[i]['class_code'] },
+                { value: info[i]['class_code'], label: `${info[i]['class_code']}: ${info[i]['class_name']}`},
             );
         }
     
@@ -111,7 +70,77 @@ function initialize_elective_classes() {
 
         for(let i = 0; i < info.length; i++) {
             classes.push(
-                { value: info[i]['class_code'], label: info[i]['class_code'] },
+                { value: info[i]['class_code'], label: `${info[i]['class_code']}: ${info[i]['class_name']}` },
+            );
+        }
+    
+        return classes;
+}
+
+export default Sidebar;*/
+
+const Sidebar = ({ onOptionSelect }) => {
+    const [selectedOption, setSelectedOption] = useState(null);
+    
+    const options1 = initialize_required_classes();
+    const options2 = initialize_elective_classes();
+
+    const handleOptionSelect = (option) => {
+        setSelectedOption(option.value);
+        onOptionSelect(option);
+    };
+
+    return (
+        <div className="sidebar">
+            <h3>Required Classes:</h3>
+            <div className="class-list">
+                {options1.map((option) => (
+                    <div
+                        key={option.value}
+                        className={`class-item ${selectedOption === option.value ? 'selected' : ''}`}
+                        onClick={() => handleOptionSelect(option)}
+                    >
+                        {option.label}
+                    </div>
+                ))}
+            </div>
+
+            <h3>Elective Classes:</h3>
+            <div className="class-list">
+                {options2.map((option) => (
+                    <div
+                        key={option.value}
+                        className={`class-item ${selectedOption === option.value ? 'selected' : ''}`}
+                        onClick={() => handleOptionSelect(option)}
+                    >
+                        {option.label}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+function initialize_required_classes() {
+    var classes = [];
+    const info = require('./RequiredClasses.json');
+
+        for(let i = 0; i < info.length; i++) {
+            classes.push(
+                { value: info[i]['class_code'], label: `${info[i]['class_code']}: ${info[i]['class_name']}`},
+            );
+        }
+    
+        return classes;
+}
+
+function initialize_elective_classes() {
+    var classes = [];
+    const info = require('./ElectiveClasses.json');
+
+        for(let i = 0; i < info.length; i++) {
+            classes.push(
+                { value: info[i]['class_code'], label: `${info[i]['class_code']}: ${info[i]['class_name']}` },
             );
         }
     
